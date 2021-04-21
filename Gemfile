8197fcad2b7755adc98ff6ad9b2b5d6e78ff30fe
cheikh <cheikh.kebe.01@gmail.com>
2021-04-21 18:39:38 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+ruby '2.7.1'
+gem 'rubocop'
+gem 'dotenv'
+gem 'twitter'
+gem 'pry'
+gem 'rspec'
---
+++ lib/bot.rb
@@ -0,0 +1,16 @@
+require 'twitter'
+require 'dotenv'
+require 'pry'
+Dotenv.load
+# n'oublie pas les lignes pour Dotenv ici…
+
+# quelques lignes qui appellent les clés d'API de ton fichier .env
+def login_twitter
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+end
+return client
+end
